33699995cc43713d6e63432568aa1d1d6b0a2537
Thiago Soares Mota <thiagomota456@gmail.com>
2018-09-12 16:43:36 -0300

    Construindo função de leitura de arquivo

--- TP01.c
+++ TP01.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 
+//Prototipos das Funções
 
-//Estruturra para cada Item
+FILE *lerArquivo(char *nome);
+
+//Tipo abstrato para as caracteristicas que um Item deve ter
 
 typedef struct item{
 
@@ -10,19 +13,66 @@ typedef struct item{
 
 }ITEM;
 
+//Função Principal
+
 int main(){
 
-	ITEM item1, item2;
-
-	item1.peso = 34;
-	item1.valor = 34;
-
-
-	item2.peso = 35;
-	item2.valor = 35;
-
-	printf("item1.peso = %d ", item1.peso);
-	printf("item2.peso = %d ", item2.peso);
+	lerArquivo("Documen!to sem título.txt\0");
+	lerArquivo("Do");
 	
 	return 0;
-}
+}//end main
+
+//Função pra abrir um arquivo para leitura
+//Recebe nome do arquivo e retorna ponteiro para Strem
+
+FILE *lerArquivo(){
+
+	FILE *PonteiroDeArquivo;
+
+	//Abrir arquivo para leitura
+
+	PonteiroDeArquivo = fopen(nomeDoArquivo, "r");
+	
+	//retorno conteudo do Ponteiro de Arquivo	
+
+	return PonteiroDeArquivo;
+	
+}//end lerArquivo
+
+void lerArquivo(ITEM *mochila, ITEM *listaDeItens, char *nomeDoArquivo){
+	
+	FILE *PonteiroDeArquivo;
+	int leituraDeCaracteresDoArquivo;
+
+	//Abri arquivo para leitura	
+
+	PonteiroDeArquivo = fopen(nomeDoArquivo, "r");
+
+	//Testa se realizoua a litura correta, 
+	
+	if(PonteiroDeArquivo == NULL){
+
+		//caso não tenha sido possivel realizar a leirura do arquivo
+
+		Mochila = NULL;
+		ListaDeItens = NULL;
+	}
+	else{
+		//Caso arquivo lido
+
+		//Ler Capacidade da Mochila
+
+		leituraDeCaracteresDoArquivo = fgetch(PonteiroDeArquivo);
+
+		//Crio mochila
+
+		mochila = (ITEM *) malloc(sizeof(ITEM * leituraDeCaracteresDoArquivo));
+
+		//Crio lista de Itens	
+	
+	}
+
+}//end lerArquivo
+
+
