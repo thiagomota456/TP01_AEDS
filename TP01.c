2357578dcff5579c8989a1eb9dafefbc74a718f4
Thiago Soares Mota <thiagomota456@gmail.com>
2018-09-13 12:09:14 -0300

    add arquivos novos e alterações

--- TP01.c
+++ TP01.c
@@ -1,57 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include "tipos.c"
-#include "leitura.c"
+#include "tipos.h"
+#include "leitura.h"
 
-//Função que le o arquivo nos padroes especificados
-// Passo como parametro respectivamente o endereço de uma variavel int que representa a capacidade da mochila, um ponteiro para lista de itens, e uma string com nome do arquivo não ha retorno. Vetor de itens é gerado e valor em CapacidadeDaMochila é gravado 
-/*
-void lerArquivo(int *CapacidadeDaMochila, ITEM *listaDeItens, char *nomeDoArquivo){	
-
-	FILE *PonteiroDeArquivo;
-	int i = 0;
-	int auxiliar;
-
-	//Abri arquivo para leitura
-
-	PonteiroDeArquivo = fopen(nomeDoArquivo, "r");
-
-	//Testa se realizoua a litura correta, 
-	
-	if(PonteiroDeArquivo == NULL){
-
-		printf("ERRO: NAO FOI POSSIVEL ABRIR O ARQUIVO\n");
-		return;
-	}
-
-	//Leio mochila
-
-	fscanf( PonteiroDeArquivo, "%d", CapacidadeDaMochila);
-
-	//Leio numero de itens
-
-	fscanf( PonteiroDeArquivo, "%d", &auxiliar);
-
-	//Crio vetor do tipo Itens
-
-	listaDeItens = (ITEM *) malloc(sizeof(ITEM) * auxiliar);
-
-	//add todos os itens ao vetor
-
-	while( fscanf( PonteiroDeArquivo, "%d %d", &listaDeItens[i].peso, &listaDeItens[i].valor) != EOF ){		
-
-			//Mostra leitura
-
-			printf("%d %d\n", listaDeItens[i].peso, listaDeItens[i].valor);
-
-			//incremento o indice pra add proximo item
-
-			i++;
-
-	}//end while EOF
-
-}//end lerArquivo
-*/
 //Função Principal
 
 int main(){
